267e6623367a6afa1372b251617bd4156b0064e8
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2022-09-23 15:05:59 +0400

    Init commit.

---
+++ v12_oracle_fdw/entry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if which "$1">/dev/null 2>&1; then
+  exec $*
+else
+  sudo /opt/pgpro/1c-12/bin/pg-wrapper links update
+  sudo mkdir -p "$PGDATA" && \
+  sudo chown "$POSTGRES_USER": "$PGDATA"
+  initdb --username="$POSTGRES_USER" --pwfile=<(echo "$POSTGRES_PASSWORD")
+  if [ "$?" = "0" ]; then
+    /opt/pgpro/1c-12/share/1c.tune > "$PGDATA"/postgresql.conf
+    echo 'host    all             all             0.0.0.0/0               md5' >> "$PGDATA"/pg_hba.conf
+  fi
+  exec postmaster $*
+fi
